5183919773bc143f242535b94e7668b1f7503d1c
sahil-1602 <sahildarji16@gmail.com>
2021-04-03 11:20:16 +0530

    completed bubble sort algorithm

---
+++ src/Algorithms/BubbleSort.js
@@ -0,0 +1,37 @@
+// export function getBubbleSortAnimation(array){
+//     let animations = [];
+//     let passedArr = array.slice();
+//     for(let i=passedArr.length; i>0; i--){
+//         var noSwaps = true;
+//         for(let j=0; j<(i-1); j++){
+//             if(passedArr[j] > passedArr[j+1]) {
+//                 animations.push([passedArr[j],passedArr[j+1]]);
+//                 let temp   = passedArr[j+1];
+//                 passedArr[j+1] = passedArr[j];
+//                 passedArr[j]   = temp;
+//                 noSwaps = false;
+//             }
+//         }
+//         if(noSwaps) break;
+//     }
+//     return animations;
+// }
+
+export function getBubbleSortAnimation(array){
+    let animations = [];
+    let passedArr = array.slice();
+    for(let i=passedArr.length; i>0; i--){
+        var noSwaps = true;
+        for(let j=0; j<(i-1); j++){
+            animations.push([passedArr[j],passedArr[j+1]]);
+            if(passedArr[j] > passedArr[j+1]) {
+                let temp   = passedArr[j+1];
+                passedArr[j+1] = passedArr[j];
+                passedArr[j]   = temp;
+                noSwaps = false;
+            }
+        }
+        if(noSwaps) break;
+    }
+    return animations;
+}
